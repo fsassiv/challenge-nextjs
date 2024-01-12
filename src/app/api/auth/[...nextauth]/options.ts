abf800ee6fab84030f6bfbcfce97aae94d7cdee7
Flavio Andrade <flavio.ssiv@gmail.com>
2024-01-12 20:56:09 -0300

    testes deploy

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,13 +1,13 @@
 import type { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import Github from "next-auth/providers/github";
+// import Github from "next-auth/providers/github";
 
 export const OPTIONS: NextAuthOptions = {
   providers: [
-    Github({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
+    // Github({
+    //   clientId: process.env.GITHUB_ID as string,
+    //   clientSecret: process.env.GITHUB_SECRET as string,
+    // }),
     Credentials({
       name: "Credentials",
       credentials: {
